a15c4e75ab1d3c29fd0094d75ba9d7e9fe44e1ee
xiewulong <xiewulong@vip.qq.com>
2017-04-15 21:21:55 +0800

    jha

--- bin/index.js
+++ bin/index.js
@@ -1,12 +1,15 @@
 #!/usr/bin/env node
-
 'use strict';
 
 const program = require('commander');
 
 const pkg = require('../package.json');
 
+program.version(pkg.version);
+
 program
-	.version(pkg.version)
-	.option('init', 'Application environment initialization')
-	.parse(process.argv);
+	.command('init [path]')
+	.action((path) => {
+	});
+
+program.parse(process.argv);
